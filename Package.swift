7c8ade11ffacee8c1e175b06933e09b030a9ecdd
Hyowon Jeon <hyowon612@gmail.com>
2026-02-09 14:46:33 +0900

    fix: point binary targets to v1.0.1 release

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "whisper",
-            url: "https://github.com/predict-woo/open-whisper-kit/releases/download/v1.0.0/whisper.xcframework.zip",
+            url: "https://github.com/predict-woo/open-whisper-kit/releases/download/v1.0.1/whisper.xcframework.zip",
             checksum: "a79725d4be9232d1963cb5e01d42b0f1c8683f501a6c46a3a87a820799488897"
         ),
         .binaryTarget(
             name: "sortformer",
-            url: "https://github.com/predict-woo/open-whisper-kit/releases/download/v1.0.0/sortformer.xcframework.zip",
+            url: "https://github.com/predict-woo/open-whisper-kit/releases/download/v1.0.1/sortformer.xcframework.zip",
             checksum: "8d0ea6afdf191d055fd1bbda89c84fd0197571ba68bd58f8a197376686485264"
         ),
         .target(
